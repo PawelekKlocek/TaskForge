bf3d291c23c55a456ea8afc1796e6f6b14bf7b96
Kamil Hebda <kamilhebda28@gmail.com>
2024-04-17 18:36:14 +0200

    add user to database

--- website/auth.py
+++ website/auth.py
@@ -1,39 +1,40 @@
 from flask import Flask, render_template, redirect, url_for, request, flash, Blueprint
-# import uuid
-# import datetime
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_login import UserMixin
-# from flask_bcrypt import Bcrypt
-# from flask_security import current_user, login_required
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
-    if request.method == 'POST':
-        username = request.form['username']
-    #     password = request.form['password']
-    #     user = User.query.filter_by(username=username).first()
-    #     if user:
-    #         if bcrypt.check_password_hash(user.password, password): #funkcja porownoje zhashowane haslo z haslem wpisanym przez uzytkownika
-    #             session['user_id'] = user.id
-    #             return redirect(url_for('home'))
-    #         else:
-    #             flash('Invalid password. Please try again.', 'error') #flash to metoda do wypisywania wiadomosci z wydarzen w aplikacji, np. errorow
-    #     else:
-    #         flash('User not found.', 'error')
-    #
-    return render_template('login.html')
+    if request.method == 'POST':# dlatego żeby wykonywał się tylko jak ejst post request
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password): #funkcja porownoje zhashowane haslo z haslem wpisanym przez uzytkownika
+                #session['user_id'] = user.id
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Invalid password. Please try again.', 'error') #flash to metoda do wypisywania wiadomosci z wydarzen w aplikacji, np. errorow
+        else:
+            flash('User not found.', 'error')
+    
+    return render_template('login.html', user=current_user)
 
 @auth.route('/logout', methods=['GET'])
+@login_required
 def logout():
     # print(session)
     # session.clear()
     # print(session)
+    logout_user()
     flash('You have been logged out!', category='success')
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('auth.login')) 
+
 
 
 @auth.route('/register', methods=['GET', 'POST'])
@@ -42,23 +43,19 @@ def register():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
         if len(username) < 2:
             flash('The username must be greater than 1 character.', category='error')
         elif len(password) < 8:
             flash('The password must be blalblabla.', category='error')
-
+        elif user:
+            flash('The username is already taken. Please choose a different one.', category='error')
         else:
-            print("utworzono konto")
+            new_user = User(username=username, password=generate_password_hash(password, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
             flash('Account created.', category='success')
+            return redirect(url_for('auth.login'))
 
-        # user = User.query.filter_by(username=username).first()
-        # if user:
-        #     flash('The username is already taken. Please choose a different one.')
-        #     return redirect(url_for('register'))
-        #     new_user = User(username=username, password=hashed_password)
-        #     db.session.add(new_user)
-        #     db.session.commit()
-        #     flash('Registration successful! You can now log in.', 'success')
-        #     return redirect(url_for('login'))
-
-    return render_template('register.html')
+    return render_template('register.html', user=current_user)

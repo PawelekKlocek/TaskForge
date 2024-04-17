7d53f3f2c2b9f320e299f5173309c0906ba47e59
Kamil Hebda <kamilhebda28@gmail.com>
2024-04-17 18:35:57 +0200

    add notes and todos to database

--- website/views.py
+++ website/views.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, redirect, url_for, request, flash, Blueprint
 import uuid
 import datetime
+from flask_login import login_required, current_user
+from .models import User, Task, Note
+from . import db
 # from flask_sqlalchemy import SQLAlchemy
 # from flask_login import UserMixin
 # from flask_bcrypt import Bcrypt
 
 views = Blueprint('views', __name__)
 
-todos = []  # lista zadań
-notes_list = []  # lista notatek
 
 # @app.route("/users")
 # def users():
@@ -19,100 +20,105 @@ notes_list = []  # lista notatek
 # to bedzie uzywane przy bazie danych, zeby sprawdzac czy dodalo poprawnie uzytkownika itd
 
 @views.route("/")
+@login_required
 def home():
     # if 'user_id' in session:
     #     user_id = session['user_id']
     #     print("welcome, ", user_id)
     # else:
     #     print("nie ma id")
-    return render_template("index.html")
+    return render_template("index.html", user=current_user)
 
 
 @views.route("/notes", methods=['GET', 'POST'])
+@login_required
 def notes():
-    return render_template("notes.html", notes=notes_list)
+    return render_template("notes.html", user=current_user, notes=Note.query.filter_by(user_id=current_user.id).all())
 
 
 @views.route("/add_note", methods=['POST'])
 def add_note():
-    note_text = request.form['note_name']
-    note_color = request.form['note_color']
-    current_id = str(uuid.uuid4())
+    if request.method == 'POST':
+        note_text = request.form.get('note_name')
+        note_color = request.form.get('note_color')
+        if note_text == '':
+            flash('Note cannot be empty!', 'error')
+            return redirect(url_for('views.notes'))
+        else:
+            new_note = Note(text=note_text, color=note_color, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
     # if 'user_id' not in session:
     #     flash('You have to be logged in!', 'error')
     #     return redirect(url_for('auth.login'))
     # else:
     #
-    notes_list.append({'text': note_text, 'color': note_color, 'id': current_id})
-    return redirect(url_for('views.notes'))
+    return redirect(url_for('views.notes', user=current_user))
 
 
 @views.route("/update_note/<string:note_id>", methods=['POST'])
 def update_note(note_id):
     if request.method == 'POST':
-        new_note = request.form["new_note_name"]
-        for note in notes_list:
-            if note['id'] == note_id:
-                note['text'] = new_note
-                break
-    return redirect(url_for('views.notes'))
+        note_edit = request.form.get('new_note_name')
+        if note_edit == '':
+            flash('Note cannot be empty!', 'error')
+            return redirect(url_for('views.notes'))
+        else:
+            note = Note.query.filter_by(id=note_id).first()
+            note.text = note_edit
+            db.session.commit()
+    return redirect(url_for('views.notes', user=current_user, notes=Note.query.filter_by(user_id=current_user.id).all()))
 
 
 @views.route("/delete_note/<string:note_id>", methods=['GET', 'POST'])
 def delete_note(note_id):
     if request.method == 'POST':
-        for note in notes_list:
-            if str(note['id']) == note_id:
-                notes_list.remove(note)
-                break
-    return redirect(url_for('views.notes'))
+        note = Note.query.filter_by(id=note_id).first()
+        db.session.delete(note)
+        db.session.commit()    
+    return redirect(url_for('views.notes', user=current_user ))
 
 
 @views.route("/todo", methods=['GET','POST'])  # dodajemy metody POST i GET do routingu który pozwoli nam na dodawanie nowych zadań do listy
+@login_required
 def todo():
     if request.method == 'POST':
-        todo_name = request.form["todo_name"]  # przyjmujemy dane z formularza do zmiennej todo_name
-        todo_priority = request.form["todo_priority"]
-        current_id = str(uuid.uuid4())  # Generujemy unikalny identyfikator UUID dla nowego zadania
-        # if 'user_id' not in session:
-        #     flash('You have to be logged in!', 'error')
-        #     return redirect(url_for('auth.login'))
-        # else:
-        todos.append({  # dodajemy nowy element do listy todos
-            'id': current_id,
-            'name': todo_name,
-            'checked': False,
-            'date': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            'priority': todo_priority
-        })
-    return render_template("todo.html", items=todos)  # przekazujemy liste todos do szablonu todo.html
+        todo_name = request.form.get('todo_name')
+        todo_priority = request.form.get('todo_priority')
+        todo_checked = False
+        if todo_name == '':
+            flash('Task cannot be empty!', 'error')
+        else:
+            new_todo = Task(text=todo_name, priority=todo_priority, user_id=current_user.id, checked=todo_checked)
+            db.session.add(new_todo)
+            db.session.commit()
+        
+    todos = Task.query.filter_by(user_id=current_user.id).all()  # Fetch todos from the database
+    return render_template("todo.html", user=current_user, todos=todos)
 
 
-@views.route("/delete/<string:todo_id>",methods=['POST'])  # dodajemy metody POST do routingu który pozwoli nam na usuwanie zadań z listy
+@views.route("/delete/<string:todo_id>", methods=['POST'])
 def delete_todo(todo_id):
-    for todo in todos:
-        if todo['id'] == todo_id:
-            todos.remove(todo)
+    todo = Task.query.filter_by(id=todo_id).first()  # Znajdź zadanie w bazie danych
+    if todo:
+        db.session.delete(todo)  # Usuń zadanie z bazy danych
+        db.session.commit()
     return redirect(url_for('views.todo'))
 
-
-# Do zmiany
 @views.route("/update/<string:todo_id>", methods=['POST'])
 def update_todo(todo_id):
     if request.method == 'POST':
         new_name = request.form["new_todo_name"]
-        for todo in todos:
-            if todo['id'] == todo_id:
-                todo['name'] = new_name
-                break
-    return redirect(url_for('views.todo'))
+        todo = Task.query.filter_by(id=todo_id).first()  # Znajdź zadanie w bazie danych
+        if todo:
+            todo.text = new_name  # Zaktualizuj nazwę zadania
+            db.session.commit()
+    return redirect(url_for('views.todo', user=current_user, todos=Task.query.filter_by(user_id=current_user.id).all()))
 
-
-# dodajemy metody POST do routingu który pozwoli nam na usuwanie zadań z listy
 @views.route("/checked/<string:todo_id>", methods=['POST'])
 def checked_todo(todo_id):
-    for todo in todos:
-        if todo['id'] == todo_id:
-            todo['checked'] = not todo['checked']  # zmieniamy status zadania na przeciwny
-            break
-    return redirect(url_for('views.todo'))
+    todo = Task.query.filter_by(id=todo_id).first()  # Znajdź zadanie w bazie danych
+    if todo:
+        todo.checked = not todo.checked  # Zmień status zadania
+        db.session.commit()
+    return redirect(url_for('views.todo', user=current_user))
